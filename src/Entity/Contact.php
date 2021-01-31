8008cf22cb25b864001b4a7b8a2477ce394cd9f2
mojtaba gheytasi <mjgheytasi@gmail.com>
2021-02-01 01:00:12 +0330

    Add Contact entity

---
+++ src/Entity/Contact.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Table;
+use Doctrine\ORM\Mapping\UniqueConstraint;
+
+/**
+ * @ORM\Entity(repositoryClass=ContactRepository::class)
+ * @Table(name="contract",uniqueConstraints={
+ *     @UniqueConstraint(name="unique_contract_id_owner_id", columns={
+ *          "contract_id", "owner_id"
+ *     })
+ * })
+ */
+class Contact
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=15)
+     */
+    private $mobile;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $contact;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="contacts")
+     * @ORM\JoinColumn(nullable=false, onDelete="CASCADE")
+     */
+    private $owner;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getMobile(): ?string
+    {
+        return $this->mobile;
+    }
+
+    public function setMobile(string $mobile): self
+    {
+        $this->mobile = $mobile;
+
+        return $this;
+    }
+
+    public function getContact(): ?User
+    {
+        return $this->contact;
+    }
+
+    public function setContact(?User $contact): self
+    {
+        $this->contact = $contact;
+
+        return $this;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): self
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+}
